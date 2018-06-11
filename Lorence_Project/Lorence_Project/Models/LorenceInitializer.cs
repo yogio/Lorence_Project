cea9633ac7fc5c454ed6a532f9004a3659885743
Guy <ohayonguy1@gmail.com>
2018-06-11 08:15:01 +0300

    trying to apply a new DbContext

---
+++ Lorence_Project/Lorence_Project/Models/LorenceInitializer.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+
+namespace Lorence_Project.Models
+{
+    public class LorenceInitializer : System.Data.Entity.DropCreateDatabaseIfModelChanges<LorenceDbContext>
+    {
+
+    }
+}
