e4661e449fecddaa0201fb0059de66019fe6bcf6
Guy <ohayonguy1@gmail.com>
2018-06-19 20:08:21 +0300

    g

---
+++ Lorence_Project/Lorence_Project/DAL/LorenceDbContext.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using System.Data.Entity;
+using Lorence_Project.Models;
+
+namespace Lorence_Project.DAL
+{
+    public class LorenceDbContext : DbContext
+    {
+        public LorenceDbContext() : base("LorenceDbContext") { }
+
+        public DbSet<Drink> Drinks { get; set; }
+        public DbSet<Sit> Sits { get; set; }
+        public DbSet<User> Users { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+        }
+    }
+}
---
+++ Lorence_Project/Lorence_Project/DAL/LorenceInitializer.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+using Lorence_Project.Models;
+
+namespace Lorence_Project.DAL
+{
+    public class LorenceInitializer : System.Data.Entity.DropCreateDatabaseAlways<LorenceDbContext>
+    {
+        protected override void Seed(LorenceDbContext context)
+        {
+
+            var drinks = new List<Drink>
+            {
+                new Drink{ DrinkName = "Beer"},
+                new Drink{DrinkName = "Wine"}
+            };
+            drinks.ForEach(d => context.Drinks.Add(d));
+            context.SaveChanges();
+
+           // base.Seed(context);
+        }
+    }
+}
