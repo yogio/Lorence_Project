ed4dfd010b3f74c6b23014de4118405243a08b18
Guy <ohayonguy1@gmail.com>
2018-06-20 15:35:04 +0300

    Re-arranged some files to their correct folders.

--- Lorence_Project/Lorence_Project/DAL/Model1.Designer.cs
+++ Lorence_Project/Lorence_Project/DAL/Model1.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'C:\Users\Gi\Source\Repos\Lorence_Project2\Lorence_Project\Lorence_Project\Model1.edmx'. 
+﻿// T4 code generation is enabled for model 'C:\Users\lonyg\Source\Repos\Lorence_Project\Lorence_Project\Lorence_Project\DAL\Model1.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
