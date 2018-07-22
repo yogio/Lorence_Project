9a967b8bca3ce1b8d2def2c78d34a5e905774994
Guy <ohayonguy1@gmail.com>
2018-07-22 12:50:11 +0300

    t

--- Lorence_Project/Lorence_Project/DAL/LorenceInitializer.cs
+++ Lorence_Project/Lorence_Project/DAL/LorenceInitializer.cs
@@ -17,7 +17,6 @@ namespace Lorence_Project.DAL
                 new Drink{ DrinkName = "Beer"},
                 new Drink{DrinkName = "Wine"},
                 new Drink{DrinkName = "Cooctail"},
-                new Drink{DrinkName = "test"}
             };
             drinks.ForEach(d => context.Drinks.Add(d));
             context.SaveChanges();
