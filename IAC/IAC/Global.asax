a188f5ad8ea2656dfe39a6d8d68ae8fd6ad4d6c0
Guy <ohayonguy1@gmail.com>
2018-07-28 18:15:36 +0300

    testing a new guide

---
+++ IAC/IAC/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="IAC.MvcApplication" Language="C#" %>
---
+++ IAC/IAC/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(IAC.Startup))]
+namespace IAC
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
